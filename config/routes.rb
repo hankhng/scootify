6a47f71fcf6eff4d6749d19a8e3c1904319dea92
Hank Hng <hankhng@hotmail.com>
2020-02-25 17:30:37 -0300

    removed date: string and added start_date and end_date

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root to: 'scooters#index'
   resources :scooters do
-    resources :bookings, only: [:index, :create, :show, :new]
+    resources :bookings, only: [:create, :show, :new]
   end
+  resources :bookings, only: [:index]
   devise_for :users
   # , :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
