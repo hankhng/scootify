9ed658d369d99272e8aef094adfe2cebd96bde2a
hernan masaji kina <kisiro@gmail.com>
2020-02-26 14:44:01 -0300

    clodinary setup.index arragnement for new imagescautiontake a landscape photo for upload

--- app/models/scooter.rb
+++ app/models/scooter.rb
@@ -1,6 +1,7 @@
 class Scooter < ApplicationRecord
   belongs_to :owner, class_name: "User"
   has_many :bookings
+  has_one_attached :photo # we might be willing to change it to many. and check how to define which one to show on prod cards that only allow one photo for the time being.
 
   validates :brand, presence: true
   validates :model, presence: true
