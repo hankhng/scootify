31a900c2b5e5ab3f74b410e14d8622d776012b7c
Thomas Van Holder <thomasvanholder@gmail.com>
2020-02-27 15:09:43 -0300

    created earnings page and adjusted controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,8 @@
 class BookingsController < ApplicationController
-   before_action :set_booking, only: [:show]
-   before_action :set_scooter, only: [:new, :create]
+ before_action :set_booking, only: [:show]
+ before_action :set_scooter, only: [:new, :create]
 
-  def index
+ def index
     # @bookings = Booking.all
     @bookings = policy_scope(Booking).order(start_date: :desc)
     @scooters = current_user.scooters
@@ -35,21 +35,26 @@ class BookingsController < ApplicationController
     authorize @bookings
   end
 
-  private
+  def earnings
+   @bookings = policy_scope(Booking).order(start_date: :desc)
+   @scooters = current_user.scooters
+ end
+
+ private
 
     # Use callbacks to share common setup or constraints between actions.
-  def set_booking
-    @booking = Booking.find(params[:id])
-    authorize @booking
-  end
+    def set_booking
+      @booking = Booking.find(params[:id])
+      authorize @booking
+    end
 
-  def set_scooter
-    @scooter = Scooter.find(params[:scooter_id])
-  end
+    def set_scooter
+      @scooter = Scooter.find(params[:scooter_id])
+    end
 
 
-  def booking_params
-    params.require(:booking).permit(:renter_id, :scooter_id, :start_date, :end_date, :booking_price, :comment)
+    def booking_params
+      params.require(:booking).permit(:renter_id, :scooter_id, :start_date, :end_date, :booking_price, :comment)
+    end
   end
-end
 
