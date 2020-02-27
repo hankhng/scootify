2cff0c7ffd2ac1a072e3957710c8fda47a7fc178
Hank Hng <hankhng@hotmail.com>
2020-02-27 15:27:12 -0300

    Datepicker and price calculator

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -5,6 +5,24 @@ import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
 flatpickr("#range_start", {
   altInput: true,
-  plugins: [new rangePlugin({ input: "#range_end"})]
-})
+  minDate: "today",
+  plugins: [new rangePlugin({ input: "#range_end"})],
+  onChange: function(selectedDates, dateStr, instance) {
+    if (selectedDates.length == 2) {
+      let startDate = document.querySelector('#range_start').value.split(" to ")[0];
+      let endDate = document.querySelector("#range_start").value.split(" to ")[1];
+
+      let daysInMilliseconds = new Date(endDate) - new Date(startDate)
+
+      let days = daysInMilliseconds / 86400000
+
+      let pricePerDay = document.querySelector('.list-group-item').innerText.split("$")[1]
+
+      let totalPrice = days * pricePerDay
+
+      let span = document.querySelector('#totalPrice');
+      span.innerText = ` $ ${totalPrice} for ${days} days`
+    }
+  }
+});
 
