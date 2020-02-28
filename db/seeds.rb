d0e02c8999e60671fe3bc600d6f17f75f3a7a8b0
hernan masaji kina <kisiro@gmail.com>
2020-02-28 12:47:41 -0300

    goto

--- db/seeds.rb
+++ db/seeds.rb
@@ -94,7 +94,7 @@ USERS[:owners].each do |owner_info|
     scooter.year = rand(2000..2020)
     scooter.price_per_day = rand(10..150)
     scooter.owner = owner
-    file = URI.open('https://source.unsplash.com/1600x900/?moto')
+    file = URI.open("https://source.unsplash.com/1600x900/?scooter&#{scooter.brand}")
     scooter.photo.attach(io: file, filename: "#{rand(1..999)}.jpeg", content_type: 'image/png')
     scooter.save!
 
