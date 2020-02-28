e7d786e2f38304f5d80d60a7e7a1e93be19fa252
Hank Hng <hankhng@hotmail.com>
2020-02-28 14:27:45 -0300

    my bookings page WIP

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -30,6 +30,7 @@ class BookingsController < ApplicationController
    @markers = [{
     lat: @booking.scooter.latitude,
     lng: @booking.scooter.longitude,
+    image_url: helpers.asset_url('scooter1.png')
   }]
 end
 
